f91a0907985a47946aadbfb71a0ee7c36398645d
Kriegslustig <github@ls7.ch>
2015-11-07 15:43:23 +0100

    Add a deploy script

---
+++ deploy.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+project_name=simple_pad
+
+cd /var/app/simple-pad
+git pull
+
+demeteorizer
+if (( ${?} > 0 )); then exit 1; fi
+
+docker build -t ${project_name} .
+if (( ${?} > 0 )); then exit 1; fi
+
+docker stop ${project_name}
+docker rm ${project_name}
+docker run -dp 80:80 --link mongo_${project_name}:mongo --name ${project_name} --restart=always ${project_name}
+
